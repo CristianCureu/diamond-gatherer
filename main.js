439aeb7b25c0affe2469f7ed3518e8b972eabf5e
Cristian <ccristi_01@yahoo.com>
2020-11-13 16:50:21 +0200

    Curs 1 - Tema 1

---
+++ main.js
@@ -0,0 +1,11 @@
+var person = {
+    age: 22,
+    name: "Cristian",
+    height: 186
+}
+
+function myFunction(numele) {
+    return "Buna, numele meu este: " + numele + "."
+  }
+console.log(myFunction("Cristi"));
+
