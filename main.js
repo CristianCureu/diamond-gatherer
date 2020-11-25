dd3112ef181d321cbd01fe923f1e6122fbf8176f
CristianCureu <74319096+CristianCureu@users.noreply.github.com>
2020-11-25 14:30:43 +0200

    Curs 1 - Tema 1

---
+++ main.js
@@ -0,0 +1,10 @@
+var person = {
+    age: 22,
+    name: "Cristian",
+    height: 186
+}
+
+function myFunction(numele) {
+    return "Buna, numele meu este: " + numele + "."
+  }
+console.log(myFunction("Cristi"));
